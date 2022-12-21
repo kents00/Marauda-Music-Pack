e950b51cee7ca53c843f8a1f47253c23fa7ab980
“kents00” <kentedoloverio24@gmail.com>
2022-12-21 12:15:45 +0800

    Update 21.12.22

---
+++ media/lua/shared/TCGMusicDefenitionsTCBoomboxMyMusicPack27.lua
@@ -0,0 +1,13 @@
+require "TCMusicDefenitions"
+
+	GlobalMusic["CassetteMaruadaEDC"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaCast"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaHusk"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaAvoidableCause"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaKilter"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaSwathe"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaTenebrous"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaThatSong"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaReckoning"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaTrash"] = "tsarcraft_music_01_62"
+	GlobalMusic["CassetteMaraudaUmbra"] = "tsarcraft_music_01_62"
